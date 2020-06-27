c12a74ee1cf3359d8e182872ae8b88277b5af393
Julia Tan <juliajtan@gmail.com>
2020-06-27 12:56:54 +0100

    Added TextInput field that can set state for current search term

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import { movie } from './mockData';
 import Result from './Result'
 
 export default class App extends React.Component {
   state = {
     movie: movie,
+    searchTerm: '',
+  }
+
+  handleSearchTermChange = searchTerm => {
+    this.setState({searchTerm})
   }
 
   render() {
     return (
-       <View style={styles.container}>
+      <View style={styles.container}>
+        <TextInput
+          placeholder="Search"
+          value={this.state.searchTerm}
+          onChangeText={this.handleSearchTermChange}
+        />
         <Result movie={this.state.movie} />
         <StatusBar style="auto" />
       </View>
