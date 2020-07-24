07402cc0f99aeeeb737c33e641aee96b407b90cd
Julia Tan <juliajtan@gmail.com>
2020-07-24 23:02:57 +0100

    Implemented basic authentication flow

--- api.js
+++ api.js
@@ -1,3 +1,18 @@
+export const login = async (username, password) => {
+  const response = await fetch("http://localhost:8000", {
+    method: 'POST',
+    headers: {'content-type': 'application/json'},
+    body: JSON.stringify({username, password}),
+  })
+
+  if (response.ok) {
+    return true
+  }
+
+  const errorMessage = await response.text()
+  throw new Error(errorMessage)
+}
+
 export const fetchMovies = async (searchTerm) => {
   const response = await fetch(`http://www.omdbapi.com/?apikey=340ef8c9&s=${searchTerm}`)
 
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,66 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { login } from '../api';
 
-const LoginScreen = ({ route, navigation }) => (
-  <View style={styles.container}>
-    <Button
-      title="Click here to log in"
-      onPress={() => navigation.navigate('LoggedIn' )}
-    />
-  </View>
-);
+export default class LoginScreen extends React.Component {
+
+  state = {
+    username: '',
+    password: '',
+    errorMessage: '',
+  }
+
+  authenticate = async () => {
+    if (this.state.username === 'username' && this.state.password === 'password') {
+      this.props.navigation.navigate('LoggedIn')
+    } else {
+      this.setState({errorMessage: 'Wrong details'})
+    }
+  }
+
+  // commenting out due to CORS error
+  // authenticate = async () => {
+  //   try {
+  //     const success = await login(this.state.username, this.state.password)
+  //     this.props.navigation.navigate('LoggedIn')
+  //   } catch (err) {
+  //     const errorMessage = err.message
+  //     this.setState({errorMessage: errorMessage})
+  //   }
+  // }
+
+  handleUsernameChange = (username) => { 
+    this.setState({username})
+    // same as: 
+    // this.setState({username: username})
+  }
+
+  handlePasswordChange = (password) => { 
+    this.setState({password})
+  }
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.error}>{this.state.errorMessage}</Text>
+        <TextInput
+          placeholder="Username"
+          value={this.state.username}
+          onChangeText={this.handleUsernameChange}
+        />
+        <TextInput
+          placeholder="Password"
+          value={this.state.password}
+          onChangeText={this.handlePasswordChange}
+        />
+        <Button
+          title="Log in"
+          onPress={this.authenticate}
+        />
+      </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -17,6 +69,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  error: {
+    color: 'red'
+  },
 });
-
-export default LoginScreen
